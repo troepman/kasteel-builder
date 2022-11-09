6bc7181dd9e1dd06987be833d47409c441a88dd1
Thore Roepman <thore.roepman@gmail.com>
2022-11-09 12:33:03 +0100

    Additional bar for make up

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ function compute(options, numberOfYears=40) {
         // Compute the income
         incomeBeer = yearlyBeerConsumption * currentState.beerContribution;
         incomeContribution = numberOfMembers * currentState.pandContribution;
-        incomeOther = otherContribution;
+        incomeOther = currentState.otherContribution;
         
         incomeInterest = interestRate * currentState.savings;
         
@@ -64,49 +64,52 @@ function extractTargetReachedYear(yearStates) {
     return targetReached
 }
 
-let myChart;
+let myChart1;
+let myChart2;
+const colors = {
+    interest: '#1d4e9a',
+    contribution: '#873e23',
+    beer: '#ECD578',
+    misc: '#b2c756',
+    target: '#888888',
+    background: '8f'
+}
+const labels = ["Interest", "Pand contribution", "Beer contribution", "Miscellaneous contribution", "Target" ]
 
-function updateChart(yearStates) {
-    const colors = {
-        interest: '#1d4e9a',
-        contribution: '#873e23',
-        beer: '#ECD578',
-        misc: '#b2c756',
-        target: '#888888',
-        background: '8f'
-    }
+function updateChart1(yearStates) {
+    
     const data = {
         labels: yearStates.map(k => k.year),
         datasets: [{
-            label:"Interest",
+            label:labels[0],
             data: yearStates.map(k => k.savings),
             backgroundColor: colors.interest + colors.background,
             borderColor: colors.interest,
             fill: true,
             order:4,
         },{
-            label:'Pand contribution',
+            label:labels[1],
             data: yearStates.map(k => k.savingsContribution),
             backgroundColor: colors.contribution  + colors.background,
             borderColor: colors.contribution,
             fill: true,
             order:1
         }, {
-            label:'Beer contribution',
+            label:labels[2],
             data: yearStates.map(k => k.savingsContribution + k.savingsBeer),
             backgroundColor: colors.beer + colors.background,
             borderColor: colors.beer,
             fill: true,
             order:2
         }, {
-            label: 'Miscellaneous contribution',
+            label:labels[3],
             data: yearStates.map(k => k.savingsContribution + k.savingsBeer + k.savingsOther),
             backgroundColor: colors.misc + colors.background,
             borderColor: colors.misc,
             fill: true,
             order: 3
         }, {
-            label:"Target",
+            label:labels[4],
             data: yearStates.map(k => k.target),
             backgroundColor: colors.target,
             borderColor: colors.target,
@@ -114,9 +117,9 @@ function updateChart(yearStates) {
         }]
     }
     
-    if (myChart) {
-        data.datasets.forEach((d, i) => myChart.data.datasets[i].data = d.data)
-        myChart.update();
+    if (myChart1) {
+        data.datasets.forEach((d, i) => myChart1.data.datasets[i].data = d.data)
+        myChart1.update();
     } else {
         const ctx = document.getElementById('result_chart').getContext('2d');
         const config = {
@@ -124,6 +127,7 @@ function updateChart(yearStates) {
             data,
             options: {
                 responsive: true,
+                maintainAspectRatio: false,
                 scales: {
                     yAxis:{
                         axis:'y',
@@ -133,7 +137,79 @@ function updateChart(yearStates) {
                 }
             }
         }
-        myChart = new Chart(ctx, config);
+        myChart1 = new Chart(ctx, config);
+    }
+}
+
+function updateChart2(yearStates) {
+    const lastYear = extractTargetReachedYear(yearStates);
+    console.log('last year', lastYear)
+
+    const data = {
+        labels: [""],
+        datasets: [{
+            barThickness: 10,
+            borderWidth: 2,
+            label: labels[1],
+            data: [lastYear.savingsContribution],
+            backgroundColor: colors.contribution + colors.background,
+            borderColor: colors.contribution,
+        },
+        {
+            barThickness: 10,
+            borderWidth: 2, 
+            label: labels[2], 
+            data:[lastYear.savingsBeer],
+            backgroundColor: colors.beer + colors.background,
+            borderColor: colors.beer,
+        },
+        {
+            barThickness: 10, 
+            borderWidth: 2, 
+            label: labels[3], 
+            data:[lastYear.savingsOther],
+            backgroundColor: colors.misc + colors.background,
+            borderColor: colors.misc,
+        },
+        {
+            barThickness: 10, 
+            borderWidth: 2, 
+            label: labels[0], 
+            data:[lastYear.savingsInterest],
+            backgroundColor: colors.interest + colors.background,
+            borderColor: colors.interest,
+        }]
+    }
+    if (myChart2) {
+        data.datasets.forEach((d, i) => myChart2.data.datasets[i].data = d.data)
+        myChart2.update();
+    } else {
+        const ctx = document.getElementById('result_chart2').getContext('2d');
+        const config = {
+            type: 'bar',
+            data,
+            options: {
+                aspectRatio: 6,
+                maintainAspectRatio: false,
+                responsive: true,
+                indexAxis: 'y',
+                plugins: {legend: {
+                    display: true
+                 },
+                },
+                scales: {
+                    yAxis:{
+                        axis:'y',
+                        stacked: true
+                    },
+                    xAxis: {
+                        axis:'x',
+                        stacked: true
+                    }
+                }
+            }
+        }
+        myChart2 = new Chart(ctx, config);
     }
 }
 
@@ -164,7 +240,8 @@ function onUpdate() {
         target, beerContribution, pandContribution, inflationRate, interestRate, yearlyBeerConsumption:yearlyBeerConsumption*100*3.33, numberOfMembers:350, otherContribution
     })
 
-    updateChart(result);
+    updateChart1(result);
+    updateChart2(result);
     updateSummary(result);
 }
 
